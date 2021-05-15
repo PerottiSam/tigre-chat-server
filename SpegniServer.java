6b1e4a89ffac15fe489445bd16563788be26a971
PerottiSam <80097352+PerottiSam@users.noreply.github.com>
2021-05-15 15:38:42 +0200

    Update SpegniServer.java

--- SpegniServer.java
+++ SpegniServer.java
@@ -1,5 +1,6 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Vector;
 
 public class SpegniServer extends Thread{
     Server server;
@@ -11,12 +12,34 @@ public class SpegniServer extends Thread{
     @Override
     public void run() {
         try{
+            String comando;
             BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+            Vector<ConnectionHandler> lista;
 
-            while (!in.readLine().equalsIgnoreCase("quit")){ }
+
+            while (!(comando = in.readLine()).equalsIgnoreCase("quit")){
+                if(comando.equalsIgnoreCase("online")){
+                    lista = server.getListaSocket();
+                    for (int i = 0; i < lista.size(); i++) {
+                        if (lista.get(i) != null){
+                            System.out.println("User: " + lista.get(i).getIo());
+                        }
+                    }
+                }else if (comando.equalsIgnoreCase("ip")){
+                    lista = server.getListaSocket();
+                    for (int i = 0; i < lista.size(); i++) {
+                        if (lista.get(i) != null){
+                            System.out.println("Socket: " + lista.get(i).mioSocket);
+                        }
+                    }
+                }
+            }
 
             //PROCEDURA CHIUSURA DI TUTTI I THREAD ANCHE DEI RISPETTIVI THREAD DI LETTURA DEI CLIENT TRAMITE L'INVIO DEL MESSAGGIO CHIUDI-THREAD
-            server.questoServer.close();
+            try{
+                server.questoServer.close();
+            }catch (Exception exxx){}
+
             for (int i = 0; i < server.listaSocket.size(); i++) {
                 try{
                     server.listaSocket.get(i).send("CHIUDI-THREAD");
